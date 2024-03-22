a2eaf77988a58d98fb720ab0850ae3c15cc0587c
fantaasyyy <henricodss@gmail.com>
2024-03-22 19:04:17 -0300

    primeiro commit da minha API OAS

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,1 @@
+using Microsoft.EntityFrameworkCore;using OASv1.Model;namespace OASv1.Data;public class ApplicationDbContext : DbContext{    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)    {    }    public DbSet<AProductModel> Produtos { get; set; }}
---
+++ Migrations/20240322220057_CreateDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OASv1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Valor = table.Column<double>(type: "REAL", nullable: false),
+                    NomeDoProduto = table.Column<string>(type: "TEXT", nullable: false),
+                    ResponsavelPelaBuscaDoProduto = table.Column<string>(type: "TEXT", nullable: false),
+                    NotaFiscal = table.Column<string>(type: "TEXT", nullable: false),
+                    Validade = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataDeRecebimento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataDeAlteracao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
