2ed7cc189a10dccf704c187d633932b70cb841f7
ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎  ㅤㅤㅤ‎ㅤㅤㅤ‎ ‎ ‎ ‎ ‎  ‎ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎ ‎ ㅤㅤㅤ‎ ‎ ‎ ‎  ego <104780505+fantaasyyy@users.noreply.github.com>
2024-03-23 09:32:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-using Microsoft.EntityFrameworkCore;using OASv1.Data;var builder = WebApplication.CreateBuilder(args);// Add services to the container.// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbucklebuilder.Services.AddEndpointsApiExplorer();builder.Services.AddSwaggerGen();builder.Services.AddControllersWithViews();builder.Services.AddDbContext<ApplicationDbContext>(options =>{    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));});var app = builder.Build();// Configure the HTTP request pipeline.if (app.Environment.IsDevelopment()){    app.UseSwagger();    app.UseSwaggerUI();}app.UseHttpsRedirection();app.MapGet("/produtos", () => "teste");app.Run();
+using Microsoft.EntityFrameworkCore;
+using OASv1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/produtos", () => "teste");
+
+app.Run();
